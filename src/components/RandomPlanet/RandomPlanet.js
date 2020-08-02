164a08383252a623f32505eac9f688895cb8fbb3
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-02 22:47:42 +0300

    feat: реализовать загрузку данных рандомных планет

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RandomPlanet.scss';
 
-function RandomPlanet() {
-  return (
-    <section className="random-planet">
-      <img
-        src="https://www.ejin.ru/wp-content/uploads/2019/05/golubaja-planeta-zemlja-800x800.jpg"
-        alt=""
-        className="random-planet__img"
-      />
-      <h2 className="random-planet__title">Planet Name</h2>
-      <ul className="random-planet__list">
-        <li className="random-planet__list-item">
-          <span className="random-planet__text">Population</span>
-        </li>
-        <li className="random-planet__list-item">
-          <span className="random-planet__text">Rotation Period 43</span>
-        </li>
-        <li className="random-planet__list-item">
-          <span className="random-planet__text">Diameter 100</span>
-        </li>
-      </ul>
-    </section>
-  );
-}
+import SwapiService from './../../utils/SwapiService';
 
-export default RandomPlanet;
+export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    id: null,
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diameter: null,
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 10 + 2);
+    this.swapiService.getPlanet({ id }).then((planetData) => {
+      this.setState({
+        id: id,
+        name: planetData.name,
+        population: planetData.population,
+        rotationPeriod: planetData.rotation_period,
+        diameter: planetData.diameter,
+      });
+    });
+  }
+
+  com;
+
+  render() {
+    const { id, name, population, rotationPeriod, diameter } = this.state;
+
+    return (
+      <section className="random-planet">
+        <img
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+          alt=""
+          className="random-planet__img"
+        />
+        <h2 className="random-planet__title">{name}</h2>
+        <ul className="random-planet__list">
+          <li className="random-planet__list-item">
+            <span className="random-planet__text">
+              Population: {population}
+            </span>
+          </li>
+          <li className="random-planet__list-item">
+            <span className="random-planet__text">
+              Rotation period: {rotationPeriod}
+            </span>
+          </li>
+          <li className="random-planet__list-item">
+            <span className="random-planet__text">Diameter: {diameter}</span>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
