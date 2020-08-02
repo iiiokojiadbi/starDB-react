ecd4de2040108d407c3d69f5e7829aafdd56584f
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-02 17:17:47 +0300

    feat: создать компонент SwapiService

---
+++ src/utils/SwapiService.js
@@ -0,0 +1,40 @@
+export default class SwapiService {
+  _urlBase = 'https://swapi.dev/api';
+
+  async getResource(url) {
+    const response = await fetch(`${this._urlBase}${url}`);
+
+    if (!response.ok) {
+      throw new Error(`Could not fetch ${url}, received ${response.status}`);
+    }
+    const body = await response.json();
+    return body;
+  }
+
+  async getAllPeople() {
+    const response = await this.getResource('/people/');
+    return response.results;
+  }
+
+  async getAllPlanets() {
+    const response = await this.getResource('/planets/');
+    return response.results;
+  }
+
+  async getAllStarships() {
+    const response = await this.getResource('/starships/');
+    return response.results;
+  }
+
+  getPerson({ id }) {
+    return this.getResource(`/people/${id}`);
+  }
+
+  getPlanet({ id }) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  getStarship({ id }) {
+    return this.getResource(`/starships/${id}`);
+  }
+}
