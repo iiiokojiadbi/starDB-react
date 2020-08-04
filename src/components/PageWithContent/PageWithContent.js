b6630c9ee3f73460bb1057e9c327ae7841bad68e
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-04 20:34:02 +0300

    feat: создать универсальный компонент для отслеживания ошибок

--- src/components/PageWithContent/PageWithContent.js
+++ src/components/PageWithContent/PageWithContent.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import './PageWithContent.scss';
 import ChooseList from './../ChooseList';
-import ErrorIndicator from './../ErrorIndicator';
+import ErrorBoundry from './../ErrorBoundry';
+
 
 const PageWithContent = ({ ItemListComponent, DetailsComponent }) => {
   class PageContent extends Component {
     state = {
       selectedPerson: 1,
-      hasError: false,
     };
-
-    componentDidCatch() {
-      this.setState({
-        hasError: true,
-      });
-    }
 
     handlePersonSelectedId = (id) => {
       this.setState({
@@ -23,21 +17,21 @@ const PageWithContent = ({ ItemListComponent, DetailsComponent }) => {
     };
 
     render() {
-      if (this.state.hasError) {
-        return <ErrorIndicator />;
-      }
-
       const { selectedPerson: id } = this.state;
 
       return (
         <section className="page-content">
-          <ChooseList
-            onItemSelected={this.handlePersonSelectedId}
-            getData={this.props.getData}
-            selectedItem={id}
-            ItemListComponent={ItemListComponent}
-          />
-          <DetailsComponent selectedItem={id} />
+          <ErrorBoundry>
+            <ChooseList
+              onItemSelected={this.handlePersonSelectedId}
+              getData={this.props.getData}
+              selectedItem={id}
+              ItemListComponent={ItemListComponent}
+            />
+          </ErrorBoundry>
+          <ErrorBoundry>
+            <DetailsComponent selectedItem={id} />
+          </ErrorBoundry>
         </section>
       );
     }
